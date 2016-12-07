b591645a3e6187b0422c5870367f7349122b154f
chriswongatcuhk <wc38.wong@gmail.com>
2016-12-07 23:08:52 +0800

    Simulate ngPluralize.

--- src/components/TodoFooter/TodoFooter.jsx
+++ src/components/TodoFooter/TodoFooter.jsx
@@ -7,7 +7,7 @@ const TodoFooter = ({
 	todos
 }) => (
 		<footer id="footer" style={(todos.size > 0) ? {display: 'block'} : {display: 'none'}}>
-			<span id="todo-count"><strong>{todos.size}</strong></span>
+			<span id="todo-count"><strong>{todos.size}</strong>{(todos.size === 1) ? ' item left' : ' items left'}</span>
 		</footer>
 );
 
