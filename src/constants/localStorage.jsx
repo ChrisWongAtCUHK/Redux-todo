f6f598ae1cc169475938c584c36e54abc388927b
chriswongatcuhk <wc38.wong@gmail.com>
2016-12-08 23:26:40 +0800

    Fix the bug of create todo.

--- src/constants/localStorage.jsx
+++ src/constants/localStorage.jsx
@@ -21,14 +21,17 @@ export const initLSTodos = (todos, todo) => {
  * */
 export const getLSTodos = (todos, todo) => {
 	let storage = JSON.parse(localStorage.getItem(STORAGE_ID) || '[]');
-	
-	todos = todos.push(todo);
-	for(let i = 0; i < storage.length; i++){
-		todo = todo.set('title', storage[i].title)
-								.set('completed', storage[i].completed)
-								.set('id', storage[i].id);
-		todos = todos.push(todo);
+
+	if(todos.size === 0){
+		for(let i = 0; i < storage.length; i++){
+			todos = todos.push(
+					todo.set('title', storage[i].title)
+							.set('completed', storage[i].completed)
+							.set('id', storage[i].id)
+			);
+		}
 	}
+	todos = todos.push(todo);
 	
 	return todos;
 };
