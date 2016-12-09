fe8e8ab073a7e1f160b9ebd95f1dbf76470d6949
chriswongatcuhk <wc38.wong@gmail.com>
2016-12-09 22:19:27 +0800

    Init todos from local storage in TodoFooter.

--- src/containers/TodoFooterContainer/TodoFooterContainer.jsx
+++ src/containers/TodoFooterContainer/TodoFooterContainer.jsx
@@ -1,12 +1,13 @@
 import { connect } from 'react-redux';
 import TodoFooter from '../../components/TodoFooter';
+import { initLSTodos } from '../../constants/localStorage.jsx';
 
 /*
  * Connect to Todo footer component
  * */
 export default connect(
 	(state) => ({
-		todos: state.getIn(['todoReducers', 'todos']),
+		todos: initLSTodos(state.getIn(['todoReducers', 'todos']),state.getIn(['todoReducers', 'todo'])),
 		selectedStatus: state.getIn(['todoReducers', 'selectedStatus'])
 	}),
 	(dispatch) =>({
