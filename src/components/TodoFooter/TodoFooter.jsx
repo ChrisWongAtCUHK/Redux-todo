ace6d0271a3d53d1ec2fd8654796da54c000623e
chriswongatcuhk <wc38.wong@gmail.com>
2016-12-09 23:36:27 +0800

    Todo footer archors class.

--- src/components/TodoFooter/TodoFooter.jsx
+++ src/components/TodoFooter/TodoFooter.jsx
@@ -8,20 +8,20 @@ const TodoFooter = ({
 	todos,
 	selectedStatus
 }) => (
-		<footer id="footer" style={(todos.size > 0) ? {display: 'block'} : {display: 'none'}}>
-			<span id="todo-count"><strong>{todos.size}</strong>{(todos.size === 1) ? ' item left' : ' items left'}</span>
-			<ul id="filters">
-				<li>
-					<a href="#/">All</a>
-				</li>
-				<li>
-					<a onClick={onChangeStatus('active')} href="#/">Active</a>
-				</li>
-				<li>
-					<a href="#/">Completed</a>
-				</li>
-			</ul>
-		</footer>
+	<footer id="footer" style={(todos.size > 0) ? {display: 'block'} : {display: 'none'}}>
+		<span id="todo-count"><strong>{todos.size}</strong>{(todos.size === 1) ? ' item left' : ' items left'}</span>
+		<ul id="filters">
+			<li>
+				<a onClick={onChangeStatus('')} className={(selectedStatus === '') ? 'selected': ''} href="#/">All</a>
+			</li>
+			<li>
+				<a onClick={onChangeStatus('active')} className={(selectedStatus === 'active') ? 'selected': ''} href="#/">Active</a>
+			</li>
+			<li>
+				<a onClick={onChangeStatus('completed')} className={(selectedStatus === 'completed') ? 'selected': ''} href="#/">Active</a>
+			</li>
+		</ul>
+	</footer>
 );
 
 export default TodoFooter;
